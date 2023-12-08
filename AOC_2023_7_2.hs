07dd8409a67341687c3299a8d1d159f79563ec3e
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2023-12-08 15:46:58 +0300

    Add files via upload

---
+++ AOC_2023_7_2.hs
@@ -0,0 +1,71 @@
+module AOC_2023_7 where
+import Data.List.Split (splitOn)
+import System.IO (readFile)
+import Data.List (permutations)
+import qualified Data.Map as Map (keys, elems, fromList)
+   
+main2 = do  
+    contents <- readFile "input.txt"  
+    putStrLn $ analyze2 contents
+
+analyze2 :: String -> String
+analyze2 s = show (count2 (splitOn "\n" s) 0)
+
+cards = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+
+hand_type :: String -> Int
+{-
+Five of a kind, where all five cards have the same label: AAAAA, 7
+Four of a kind, where four cards have the same label and one card has a different label: AA8AA, 6
+Full house, where three cards have the same label, and the remaining two cards share a different label: 23332, 5
+Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98, 4
+Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432,3
+One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4, 2
+High card, where all cards' labels are distinct: 23456, 1
+-}
+hand_type x  | (l == 5) = 1
+             | l == 4 = 2
+             | elem (f x) (permutations [2,2,1]) = 3
+             | elem (f x) (permutations [3,1,1]) = 4
+             | (f x == [2,3]) || (f x == [3,2]) = 5
+             | (f x == [1,4]) || (f x == [4,1]) = 6
+             | l == 1 = 7
+             | otherwise = 0
+                where f x = filter (/= 0) (map length (map (\c -> filter (== c) x) cards))
+                      l = length (f x)
+                      
+hand_type' :: String -> Int
+hand_type' x = maximum (map hand_type (map (\c -> replace' 'J' c x) cards))
+
+replace' :: Char -> Char -> String -> String
+replace' a b x = map f x
+                    where f c | c == a = b
+                              | otherwise = c
+
+helper :: [String] -> [[(String, Int)]] -> [[(String, Int)]]
+helper [] acc = acc
+helper (x : xs) acc = let x' = splitOn " " x
+                          k = x' !! 0
+                          n = read (x' !! 1) :: Int
+                          t' = (hand_type' k) - 1
+                          acc1 = map (\x -> acc !! x) [0 .. t' - 1]
+                          acc2 = [(acc !! t') ++ [((replace k), n)]] 
+                          acc3 = map (\x -> acc !! x) [t' + 1 .. 6]
+                          acc' = acc1 ++ acc2 ++ acc3 
+                                in helper xs acc'
+
+count2 :: [String] -> Int -> Int
+count2 x acc = let h = filter (/= []) (helper x (take 7 $ repeat []))
+                   maps = map (Map.fromList) h
+                   keys = concatMap Map.keys maps
+                   elems = concatMap Map.elems maps
+                        in sum $ map (\(a, b) -> a * b) $ zip elems [1 .. (length elems)]
+ 
+replace :: String -> String
+replace x = map f x
+                where f c | c == 'T' = 'A'
+                          | c == 'J' = '1'
+                          | c == 'Q' = 'C'
+                          | c == 'K' = 'D'
+                          | c == 'A' = 'E' 
+                          | otherwise = c
