e54508702b2b9abb1e63202f989cded6399dcd8a
greenlightofforgiveness <119353123+greenlightofforgiveness@users.noreply.github.com>
2024-06-10 00:04:10 +0700

    Update AOC_2023_5_1.hs

--- AOC_2023_5_1.hs
+++ AOC_2023_5_1.hs
@@ -1,8 +1,10 @@
 module AOC_2023_5 where
-import System.IO     
+import System.IO
 import Data.List.Split (splitOn)
 import System.IO (readFile)
 import qualified Data.Map as Map
+import Data.List (find)
+import Data.Maybe (fromJust)
     
 main1 = do  
     contents <- readFile "input.txt"  
@@ -16,22 +18,21 @@ analyze1 s = let x = filter (/= []) $ splitOn "\n" s
 
 count1 :: [Int] -> Map.Map String [(Int, Int, Int)] -> [Int] -> Int
 count1 [] m acc 	  =  minimum acc
-count1 (x : xs) m acc = let f n (x, y, z) = if ((n >= y) && (n < y + z)) then (x + n - y) else (-1);
-							soil' = filter (/= (-1)) $ map (f x) (m Map.! "seed-to-soil map");
-							soil = if (soil' /= []) then soil'!!0 else x;
-							fert' = filter (/= (-1)) $ map (f soil) (m Map.! "soil-to-fertilizer map");
-							fert = if (fert' /= []) then fert'!!0 else soil;
-							water' = filter (/= (-1)) $ map (f fert) (m Map.! "fertilizer-to-water map");
-							water = if (water' /= []) then water'!!0 else fert;
-							light' = filter (/= (-1)) $ map (f water) (m Map.! "water-to-light map");
-							light = if (light' /= []) then light'!!0 else water;		
-							temp' = filter (/= (-1)) $ map (f light) (m Map.! "light-to-temperature map");
-							temp = if (temp' /= []) then temp'!!0 else light;
-							hum' = filter (/= (-1)) $ map (f temp) (m Map.! "temperature-to-humidity map");
-							hum = if (hum' /= []) then hum'!!0 else temp;
-							loc' = filter (/= (-1)) $ map (f hum) (m Map.! "humidity-to-location map");
-							loc = if (loc' /= []) then loc'!!0 else hum;
-								in count1 xs m (acc ++ [loc])
+count1 (x : xs) m acc = let     soil' = find (\(a, b, c) -> (x >= b) && (x < b + c)) (m Map.! "seed-to-soil map")
+                                soil = if (soil' /= Nothing) then let (a, b, c) = fromJust soil' in a + x - b else x
+                                fert' = find (\(a, b, c) -> (soil >= b) && (soil < b + c)) (m Map.! "soil-to-fertilizer map")
+                                fert = if (fert' /= Nothing) then let (a, b, c) = fromJust fert' in a + soil - b else soil
+                                water' = find (\(a, b, c) -> (fert >= b) && (fert < b + c)) (m Map.! "fertilizer-to-water map")
+                                water = if (water' /= Nothing) then let (a, b, c) = fromJust water' in a + fert - b else fert
+                                light' = find (\(a, b, c) -> (water >= b) && (water < b + c)) (m Map.! "water-to-light map")
+                                light = if (light' /= Nothing) then let (a, b, c) = fromJust light' in a + water - b else water
+                                temp' = find (\(a, b, c) -> (light >= b) && (light < b + c)) (m Map.! "light-to-temperature map")
+                                temp = if (temp' /= Nothing) then let (a, b, c) = fromJust temp' in a + light - b else light
+                                hum' = find (\(a, b, c) -> (temp >= b) && (temp < b + c)) (m Map.! "temperature-to-humidity map")
+                                hum = if (hum' /= Nothing) then let (a, b, c) = fromJust hum' in a + temp - b else temp
+                                loc' = find (\(a, b, c) -> (hum >= b) && (hum < b + c)) (m Map.! "humidity-to-location map")
+                                loc = if (loc' /= Nothing) then let (a, b, c) = fromJust loc' in a + hum - b else hum
+                                        in count1 xs m (acc ++ [loc])
 
 helper :: [String] -> String -> [(String, [(Int, Int, Int)])] -> [(String, [(Int, Int, Int)])]
 helper [] _ acc          = acc
